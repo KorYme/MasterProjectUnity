0ce815062e6a9b6395acc9273ca8ae323a5747b7
KorYme <92892240+KorYme@users.noreply.github.com>
2025-08-07 17:04:42 +0200

    [PROG] [Events] : Load and Save events for GenericDataSaveHandler

--- Packages/SaveSystem/Runtime/GenericDataSaveHandler.cs
+++ Packages/SaveSystem/Runtime/GenericDataSaveHandler.cs
@@ -6,6 +6,25 @@ using UnityEngine;
 
 namespace SaveSystem
 {
+    [Flags]
+    public enum SaveEvent
+    {
+        None = 0,
+        OnApplicationQuit = 1 << 0,
+        OnApplicationLostFocus = 1 << 1,
+        OnDestroy = 1 << 2,
+    }
+
+    [Flags]
+    public enum LoadEvent
+    {
+        None = 0,
+        Awake = 1 << 0,
+        Start = 1 << 1,
+        OnEnable = 1 << 2,
+        OnApplicationGainFocus = 1 << 3
+    }
+    
     public class GenericDataSaveHandler<T> : MonoBehaviour where T : new()
     {
         #region FIELDS
@@ -15,45 +34,11 @@ namespace SaveSystem
         [SerializeField] protected DataSaveFileHandler<T> _dataSaveFileHandler;
 
         [Header("InGame Parameters")]
-        [SerializeField] protected bool _saveOnQuit;
+        [SerializeField] protected SaveEvent _saveEvent = SaveEvent.OnApplicationQuit;
+        [SerializeField] protected LoadEvent _loadEvent = LoadEvent.Awake;
         #endregion
 
         #region METHODS
-        protected virtual void Awake()
-        {
-            LoadGame();
-        }
-
-#if UNITY_EDITOR
-        protected virtual void Reset()
-        {
-            _saveOnQuit = true;
-        }
-#endif
-
-#if UNITY_EDITOR || UNITY_STANDALONE
-        protected virtual void OnApplicationQuit()
-        {
-            if (!_saveOnQuit) return;
-            SaveGame();
-        }
-#endif
-
-#if UNITY_ANDROID || UNITY_IOS
-        protected virtual void OnApplicationFocus(bool focus)
-        {
-            if (!m_saveOnQuit) return;
-            if (focus)
-            {
-                LoadGame();
-            }
-            else
-            {
-                SaveGame();
-            }
-        }
-#endif
-        
         public void LoadGame()
         {
             if (!_dataSaveFileHandler.TryLoad(out T data))
@@ -64,15 +49,77 @@ namespace SaveSystem
             }
             GameData = data;
         }
-
+        
+        public void SaveGame()
+        {
+            _dataSaveFileHandler.Save(GameData);
+        }
+        
         public void GenerateNewGameData()
         {
             GameData = new T();
         }
         
-        public void SaveGame()
+        protected virtual void Awake()
         {
-            _dataSaveFileHandler.Save(GameData);
+            if (_loadEvent.HasFlag(LoadEvent.Awake))
+            {
+                LoadGame();
+            }
+        }
+
+        protected virtual void Start()
+        {
+            if (_loadEvent.HasFlag(LoadEvent.Start))
+            {
+                LoadGame();
+            }
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (_loadEvent.HasFlag(LoadEvent.OnEnable))
+            {
+                LoadGame();
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_saveEvent.HasFlag(SaveEvent.OnDestroy))
+            {
+                SaveGame();
+            }
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+#if UNITY_EDITOR || UNITY_STANDALONE
+            if (_saveEvent.HasFlag(SaveEvent.OnApplicationQuit))
+            {
+                SaveGame();
+            }
+#endif
+        }
+
+        protected virtual void OnApplicationFocus(bool focus)
+        {
+            if (focus && _loadEvent.HasFlag(LoadEvent.OnApplicationGainFocus))
+            {
+                LoadGame();
+            }
+#if !UNITY_ANDROID || UNITY_IOS
+            // OnApplicationQuit is not called on mobile devices
+            else if (!focus && (_saveEvent.HasFlag(SaveEvent.OnApplicationLostFocus) || _saveEvent.HasFlag(SaveEvent.OnApplicationQuit)))
+            {
+                SaveGame();
+            }
+#else
+            else if (!focus && _saveEvent.HasFlag(SaveEvent.OnApplicationLostFocus))
+            {
+                SaveGame();
+            }
+#endif
         }
         #endregion
     }
